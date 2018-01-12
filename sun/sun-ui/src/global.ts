712df5a97800c9ee608be3e48aedf553855e0195
MikelKasneci <m.kasneci@evolutivo.it>
2018-01-12 11:56:44 +0100

    sun-ui. Created routing for profile

--- sun/sun-ui/src/global.ts
+++ sun/sun-ui/src/global.ts
@@ -1,5 +1,5 @@
 export const apConfig = Object.freeze({
   LOGO_URL: '/assets/images/login-images/nethesis-logo.png',
   MAIN_TITLE: 'Icaro',
-  API_ENDPOINT_URL: ''
+  API_ENDPOINT_URL: 'http://hstest.neth.eu:8080/api'
 });
