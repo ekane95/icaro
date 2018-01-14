b68bccf77625f5d0e9e74e04df8ebfff28aa16a4
Mikel Kasneci <mikelkasneci@hotmail.com>
2018-01-15 00:55:14 +0100

    sun-ui. Modified profile component service

--- sun/sun-ui/src/app/profile/profile.component.ts
+++ sun/sun-ui/src/app/profile/profile.component.ts
@@ -1,6 +1,9 @@
+import { NgForm } from '@angular/forms';
 import { ProfileService } from './../services/profile.service';
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { CardConfig, InfoStatusCardConfig } from 'patternfly-ng';
+import { Account, AccountFull } from '../interfaces/account';
+import { error } from 'util';
 
 @Component({
   encapsulation: ViewEncapsulation.None,
@@ -10,23 +13,61 @@ import { CardConfig, InfoStatusCardConfig } from 'patternfly-ng';
 })
 export class ProfileComponent implements OnInit {
   config: CardConfig;
-
-  constructor(private profileService: ProfileService) {}
+  model: any = {};
+  items: any[] = [];
+  account: Account = {};
+  fullAccount: AccountFull = {};
+  password: string;
+  constructor(private profileService: ProfileService) { }
 
   ngOnInit() {
     this.config = {
       noPadding: false,
       topBorder: true
     } as CardConfig;
+    this.getAllAccounts();
+  }
+
+  private postAccount() {
+    this.fullAccount.email = 'mikel@hotmail.com';
+    this.fullAccount.name = 'Mikel';
+    this.fullAccount.username = 'Mikel';
+    this.fullAccount.uuid = '29';
+    this.fullAccount.hotspotid = '29';
+    this.fullAccount.password = 'password';
+    this.fullAccount.type = 'type1';
+
+    this.profileService.postAccount(this.fullAccount).subscribe(
+      data => { console.log(data) },
+      error => { console.log(error) }
+    );
+  }
+
+
+  private changePassword(f: NgForm) {
+    this.account.email = 'mikel@hotmail.com';
+    this.account.name = 'Mikel';
+    this.account.username = 'Mikel';
+    this.account.password = this.model.newpassword;
+
+    this.profileService.changePassword(this.account).subscribe(
+      data => { console.log(data) },
+      error => { console.log(error) }
+    );
   }
 
   private getAllAccounts() {
     this.profileService.getAccounts().subscribe(
       data => {
-        console.log(data);
+        let myArray = [];
+        for (let key in data) {
+          myArray.push(data[key]);
+        }
+        this.items = myArray;
+        console.log(this.items);
       },
       error => {
-        console.log(error);
+        console.log(error.json());
       }
     );
   }
--- sun/sun-ui/src/app/services/profile.service.ts
+++ sun/sun-ui/src/app/services/profile.service.ts
@@ -1,23 +1,36 @@
+import { HeaderService } from './../header/header.service';
 import { RequestOptions, Http, Headers } from '@angular/http';
 import { Injectable } from '@angular/core';
 import { apConfig } from '../../global';
+import { Account, AccountFull } from '../interfaces/account';
 
 @Injectable()
 export class ProfileService {
   private apiAccountsUrl = apConfig.API_ENDPOINT_URL + '/accounts';
   private authenticationToken = localStorage.getItem('authenticationToken');
 
-  constructor(private http: Http) {}
+  constructor(private http: Http, private headerService: HeaderService) { }
 
   getAccounts() {
-    let myHeaders = new Headers();
-    myHeaders.append('Content-Type', 'application/json');
-    myHeaders.append('Token', this.authenticationToken);
-    let myParams = new URLSearchParams();
-    let options = new RequestOptions({ headers: myHeaders, params: myParams });
-    console.log(myHeaders);
     return this.http
-      .get(this.apiAccountsUrl, options)
-      .map(res => res);
+      .get(this.apiAccountsUrl, this.headerService.setHeader())
+      .map(res => res.json());
   }
+
+
+  changePassword(account: Account) {
+    let body = JSON.stringify(account);
+    return this.http.put(this.apiAccountsUrl + '/' + '5', body, this.headerService.setHeader())
+      .map(res => res.json());
+
+  }
+
+  postAccount(account: AccountFull) {
+    let body = JSON.stringify(account);
+    return this.http.post(this.apiAccountsUrl, body, this.headerService.setHeader())
+      .map(res => res.json());
+
+  }
+
+
 }
